a7ac63a1916200b6ce7a2b90a5e6026bee41ed01
zealmoney <chatwithzeal@gmail.com>
2024-01-01 01:31:36 -0600

    first commit

--- src/mobile/HomeMobile.js
+++ src/mobile/HomeMobile.js
@@ -1,11 +1,14 @@
 import { Sidebar } from "semantic-ui-react"
 import { NavbarMobile } from "./NavbarMobile"
+import { Footer } from "../common/Footer"
 
 export const HomeMobile = () => {
     return(
         <>
             <Sidebar.Pushable>
                 <NavbarMobile />
+                <br/><br/><br/><br/>
+                <Footer />
             </Sidebar.Pushable>
         </>
     )
