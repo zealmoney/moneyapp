69c5fd7ae722824772ad9eda83031b54af1d938f
zealmoney <chatwithzeal@gmail.com>
2024-01-06 14:55:46 -0600

    first commit

--- src/common/FindOut.js
+++ src/common/FindOut.js
@@ -14,26 +14,26 @@ export const FindOut = () => {
                         </Grid.Row>
                         <Grid.Row>
                             <Grid.Column textAlign="center" width={4}>
-                                <Icon inverted color="green" size="large" name="list" />
-                                <Header style={{fontSize: '2em'}} content='Peace of mind' />
+                                <Icon inverted color="green" size="big" name="list" />
+                                <Header style={{fontSize: '1.6em'}} content='Peace of mind' />
                                 <Header 
-                                    as='h3' 
+                                    as='h4' 
                                     content='You and your loved ones can track your transfer every step of the way.' 
                                 />
                             </Grid.Column>
                             <Grid.Column textAlign="center" width={4}>
-                                <Icon inverted color="green" size="large" name="money" />
-                                <Header style={{fontSize: '2em'}} content='Great Value' />
+                                <Icon inverted color="green" size="big" name="money" />
+                                <Header style={{fontSize: '1.6em'}} content='Great Value' />
                                 <Header 
-                                    as='h3' 
+                                    as='h4' 
                                     content='More money makes it home thanks to our great rates, special offers, and no hidden fees.' 
                                 />
                             </Grid.Column>
                             <Grid.Column textAlign="center" width={4}>
-                            <Icon inverted color="green" size="large" name="check circle" />
-                                <Header style={{fontSize: '2em'}} content='Delivery Time Guaranteed' />
+                            <Icon inverted color="green" size="big" name="check circle" />
+                                <Header style={{fontSize: '1.6em'}} content='Delivery Time Guaranteed' />
                                 <Header 
-                                    as='h3' 
+                                    as='h4' 
                                     content='You can trust that transfers will be delivered on time or we’ll refund your fees.' 
                                 />
                             </Grid.Column>
