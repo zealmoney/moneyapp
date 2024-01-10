b4100cd8eb2d5b0d21695095fdf5a5cc49043e11
zealmoney <chatwithzeal@gmail.com>
2024-01-10 12:32:31 -0600

    first commit

--- src/common/Customers.js
+++ src/common/Customers.js
@@ -22,13 +22,41 @@ export const Customers = () => {
                                         Affordable rate and convenient arrangement 
                                         for recepient! Also, very good exchange rate!
                                     </p>
-
-
                                 </Segment>
                             </Grid.Column>
-                            <Grid.Column width={4}></Grid.Column>
-                            <Grid.Column width={4}></Grid.Column>
-                            <Grid.Column width={4}></Grid.Column>
+                            <Grid.Column width={4}>
+                                <Segment circular raised inverted color="green">
+                                    <Header content='Adam Smith' />
+                                    <Rating icon="star" maxRating={3} />
+                                    <p>
+                                        Exceptionally easy application to use. 
+                                        Affordable rate and convenient arrangement 
+                                        for recepient! Also, very good exchange rate!
+                                    </p>
+                                </Segment>
+                            </Grid.Column>
+                            <Grid.Column width={4}>
+                                <Segment circular raised inverted color="green">
+                                    <Header content='Adam Smith' />
+                                    <Rating icon="star" maxRating={3} />
+                                    <p>
+                                        Exceptionally easy application to use. 
+                                        Affordable rate and convenient arrangement 
+                                        for recepient! Also, very good exchange rate!
+                                    </p>
+                                </Segment>
+                            </Grid.Column>
+                            <Grid.Column width={4}>
+                                <Segment circular raised inverted color="green">
+                                    <Header content='Adam Smith' />
+                                    <Rating icon="star" maxRating={3} />
+                                    <p>
+                                        Exceptionally easy application to use. 
+                                        Affordable rate and convenient arrangement 
+                                        for recepient! Also, very good exchange rate!
+                                    </p>
+                                </Segment>
+                            </Grid.Column>
                         </Grid.Row>
                     </Grid>
                 </Container>
