ada734807f8d434e1fbd772c565ae8331dbbde33
zealmoney <chatwithzeal@gmail.com>
2024-01-12 12:40:33 -0600

    first commit

--- src/mobile/CustomersMobile.js
+++ src/mobile/CustomersMobile.js
@@ -6,15 +6,15 @@ export const CustomersMobile = () => {
         <>
             <Segment vertical size="massive" style={{padding: '2em 0em'}}>
                 <Container>
-                    <Grid stackable>
+                    <Grid>
                         <Grid.Row>
                             <Grid.Column textAlign="center">
                                 <Header as='h1' content='What our Customers are saying' />
                             </Grid.Column>  
                         </Grid.Row>
                         <Grid.Row>
-                            <Grid.Column width={4}>
-                                <Segment circular raised>
+                            <Grid.Column width={8}>
+                                <Segment raised>
                                     <Header content='Adam Smith' />
                                     <Rating icon="star" defaultRating={4} maxRating={5} />
                                     <p>
@@ -24,8 +24,8 @@ export const CustomersMobile = () => {
                                     </p>
                                 </Segment>
                             </Grid.Column>
-                            <Grid.Column width={4}>
-                                <Segment circular raised>
+                            <Grid.Column width={8}>
+                                <Segment raised>
                                     <Header content='Adam Smith' />
                                     <Rating icon="star" defaultRating={3} maxRating={5} />
                                     <p>
@@ -35,8 +35,10 @@ export const CustomersMobile = () => {
                                     </p>
                                 </Segment>
                             </Grid.Column>
-                            <Grid.Column width={4}>
-                                <Segment circular raised>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={8}>
+                                <Segment raised>
                                     <Header content='Adam Smith' />
                                     <Rating icon="star" defaultRating={2} maxRating={5} />
                                     <p>
@@ -46,8 +48,8 @@ export const CustomersMobile = () => {
                                     </p>
                                 </Segment>
                             </Grid.Column>
-                            <Grid.Column width={4}>
-                                <Segment circular raised>
+                            <Grid.Column width={8}>
+                                <Segment raised>
                                     <Header content='Adam Smith' />
                                     <Rating icon="star" defaultRating={5} maxRating={5} />
                                     <p>
